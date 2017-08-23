8c105ac985e34002f06142a35f43afcbbff12b49
Joe Bianchi <joseph.bianchi3@gmail.com>
2017-08-23 17:11:42 -0400

    done

--- server.rb
+++ server.rb
@@ -9,5 +9,20 @@ get '/portfolio' do
 end
 
 get '/about_me' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :about_me
+end
+
+get '/favorites' do
+  @fav_links = ['http://www.google.com', 'http://www.yahoo.com', 'http://www.msn.com']
+  erb :favorites
+end
+
+get '/' do
+  redirect to('/home')
+end
+
+get '/gallery' do
+  redirect to('/portfolio')
 end
